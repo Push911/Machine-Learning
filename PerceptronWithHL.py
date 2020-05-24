e6a7830e0e8398671e79e5284ee61ff324efff3f
Push911 <49253201+Push911@users.noreply.github.com>
2020-05-24 15:05:04 +0200

    Added prediction and some testing conclusions
    While we using relu function to predict output it works very bad, but when we use sigmoid func to predict and relu derivative to find an adjustment of input-output it works quite fine.

--- PerceptronWithHL.py
+++ PerceptronWithHL.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-out = 0
-learningRate = 0.5
+predictedOutput = 0
 np.random.seed(1)
 
 trainInput = np.array([[0, 0, 1],
@@ -33,52 +32,33 @@ def reluDerivative(x):
 
 for i in range(2000):
     # Feedforward
-    # inputLayerHid = trainInput
-    matrixHidden = np.dot(trainInput, synapticWeightsHidden)
-    outHidden = sigmoid(matrixHidden)
+    hiddenLayerWeights = np.dot(trainInput, synapticWeightsHidden)
+    predictedHiddenOutput = sigmoid(hiddenLayerWeights)
 
-    matrixOutput = np.dot(outHidden, synapticWeightsOutput)
-    out = sigmoid(matrixOutput)
+    outputLayerWeights = np.dot(predictedHiddenOutput, synapticWeightsOutput)
+    predictedOutput = sigmoid(outputLayerWeights)
 
-    # Back propagation of hidden layer
-    errorOutput = out - trainOutput
-    derivativeHidden = sigmoidDerivative(matrixOutput)
-    oh = outHidden
-    adjustmentHidden = np.dot(oh.T, errorOutput * derivativeHidden)
-    # Back propagation of output layer
+    # Back propagation of hidden-output layer
+    errorOutput = predictedOutput - trainOutput
+    derivativeHidden = reluDerivative(outputLayerWeights)
+    pho = predictedHiddenOutput
+    adjustmentHidden = np.dot(pho.T, errorOutput * derivativeHidden)
+    # Back propagation of input-hidden layer
     inputLayerOut = trainInput
     erdh = errorOutput * derivativeHidden
     swo = synapticWeightsOutput
-    matr = np.dot(erdh, swo.T)
-    derivativeOutput = sigmoidDerivative(matrixHidden)
-    adjustmentOutput = np.dot(inputLayerOut.T, derivativeOutput * matr)
+    hiddenLayerError = np.dot(erdh, swo.T)
+    derivativeOutput = reluDerivative(hiddenLayerWeights)
+    adjustmentOutput = np.dot(inputLayerOut.T, derivativeOutput * hiddenLayerError)
 
     synapticWeightsOutput -= adjustmentHidden
     synapticWeightsHidden -= adjustmentOutput
-    # # Feedforward
-    # inputLayer = trainInput
-    # hiddenOutputs = sigmoid(np.dot(inputLayer, synapticWeightsHidden))
-    # outputs = sigmoid(np.dot(hiddenOutputs, synapticWeightsOutput))
-    # # Back propagation of hidden layer
-    # errorHidden = trainOutput - outputs
-    # adjustmentHidden = np.dot(hiddenOutputs.T, errorHidden * sigmoidDerivative(outputs))
-    # # Back propagation of output layer
-    # error = trainOutput - outputs
-    # adjustment = np.dot(inputLayer.T, error * sigmoidDerivative(outputs))
-    # synapticWeightsOutput += adjustment
 
 print("Weights:", synapticWeightsOutput)
-print("Output:", out)
+print("Output:", predictedOutput)
 
-# Test new situation
-# nums = []
-# inp = int(input("Please enter size of array: "))
-# for _ in range(inp):
-#     num = int(input("Please enter number: "))
-#     nums.append(num)
-
-# newInput = np.array(nums)
-# newInput = np.array([1, 1, 0])
-# output = sigmoid(np.dot(newInput, synapticWeightsOutput))
-#
-# print("New situation with same weights:", output)
+newInput = np.array([1, 1, 0]), np.array([0, 1, 1])
+for newInput in newInput:
+    hidLayer = sigmoid(np.dot(newInput, synapticWeightsHidden))
+    output = sigmoid(np.dot(hidLayer, synapticWeightsOutput))
+    print("New situation {}, with same weights: ".format(newInput), output)
